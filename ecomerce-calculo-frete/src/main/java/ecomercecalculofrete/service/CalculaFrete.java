1ce85a15f0e9696f5b74fde37ce4f44af5fb99d9
Mayck Lima <mayckclima@gmail.com>
2020-05-15 19:21:55 -0300

    microserviço de frete adicionado

--- ecomerce-calculo-frete/src/main/java/ecomercecalculofrete/service/CalculaFrete.java
+++ ecomerce-calculo-frete/src/main/java/ecomercecalculofrete/service/CalculaFrete.java
@@ -10,7 +10,7 @@ public class CalculaFrete {
 
     public double valorFreteCalculado(int totalItens)
     {
-        var frete = (Math.random() * ((10 - 5) + 1)) + 5;
+        var frete = (Math.random() * ((9 - 5) + 1)) + 5;
         System.out.println(frete);
 
         return frete * totalItens;
